72f4b413a198ac523502173d337c026cfae8d5d6
jairocaracas <jaercafe@hotmail.com>
2023-07-18 13:31:58 -0500

    third commit
    se agregó el código de la tarea 3 del modulo

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,19 +6,46 @@ const step = document.querySelector('input[name="step"]');
 const amount = document.querySelector('input[name="amount"]');
 const submit = document.querySelector('button[type="submit"]');
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+submit.addEventListener('click', submitPromise);
+
+function submitPromise(e) {
+  e.preventDefault();
+
+  if (delay.value <= 0 || step.value <= 0 || amount.value <= 0) {
+    Notiflix.Notify.warning(
+      'Por favor ingrese un valor correcto en todos los campos'
+    );
   } else {
-    // Reject
+    for (i = 0; i < amount.value; i++) {
+      delayIncresing = Number(delay.value) + i * Number(step.value);
+      createPromise(i + 1, delayIncresing)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
   }
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
   });
+}
